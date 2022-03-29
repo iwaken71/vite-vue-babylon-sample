14c596bb040668c9f5e143a6da3ed5a9791f34d7
iwaken71 <iwaken59371518@gmail.com>
2022-03-30 03:09:30 +0900

    margin 0

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,6 @@ import BabylonScene from './components/BabylonScene.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
